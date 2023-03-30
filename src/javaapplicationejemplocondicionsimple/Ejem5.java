e8d64623e91e39c99f1488c3af7db1103bc7eb20
RodriCtes12 <rodrigogut2030@outlook.com>
2023-03-30 14:07:55 -0300

    evio uno

---
+++ src/javaapplicationejemplocondicionsimple/Ejem5.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationejemplocondicionsimple;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rodrigo y Ale
+ */
+public class JavaApplicationEjemploCondicionSimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+        public static void main(String[] args) { 
+            Scanner entrada = new Scanner(System.in);
+                     int num1 = 0, num2 = 0; 
+            System.out.println("Ingreda dos numeros");
+           
+            num1= entrada.nextInt();
+            num2=entrada.nextInt();
+
+if (num1 >  25);
+System.out.println("La variable num1 aloja un número menor a la variable num2");
+System.out.println("La variable num1 aloja un número mayor a la variable num2");
+}
+    
+
+
+
+
+    }
+    
+
